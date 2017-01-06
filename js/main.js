eed33148d2e198790be2cdb8a7d25bbf540764ca
alex_santos <alex.santos@processmaker.com>
2017-01-06 12:02:59 -0400

    solid-ref
    second commit
    
    three

--- js/main.js
+++ js/main.js
@@ -1,45 +1,63 @@
 function Product(id, description) {
-    this.getId = function () {
+    this.getId = function() {
         return id;
     };
-    this.getDescription = function () {
+    this.getDescription = function() {
         return description;
     };
 }
 
-function Cart(eventAggregator) {
+function Cart() {
     var items = [];
 
-    this.addItem = function (item) {
+    this.addItem = function(item) {
         items.push(item);
+    };
+
+    this.removeItem = function (item) {
+        var index = items.indexOf(item);
+        if (index !== -1) {
+            items.splice(index, 1);
+        }
+    };
+
+    this.appendItemHtml = function(product) {
+        var newItem = $('<li></li>')
+            .html(product.getDescription())
+            .attr('id-cart', product.getId());
+        $("#cart").append(newItem);
+    };
+
+    this.removeItemHtml = function(product) {
+        var productId =  product.getId();
+        if (items.productId) {
+
+        }
     };
 }
 
 var products = [
-new Product(1, "Star Wars Lego Ship"),
-new Product(2, "Barbie Doll"),
-new Product(3, "Remote Control Airplane")],
+        new Product(1, "Star Wars Lego Ship"),
+        new Product(2, "Barbie Doll"),
+        new Product(3, "Remote Control Airplane")
+    ],
     cart = new Cart();
 
-(function () {
+(function() {
     function addToCart() {
-        var productId = $(this).attr('id');
-        var product = $.grep(products, function (x) {
-            return x.getId() == productId;
-        })[0];
+        var productId = $(this).attr('id'),
+            product = $.grep(products, function(x) {
+                return x.getId() == productId;
+            })[0];            
         cart.addItem(product);
-
-        var newItem = $('<li></li>')
-            .html(product.getDescription())
-            .attr('id-cart', product.getId())
-            .appendTo("#cart");
+        cart.appendItemHtml(product);
     }
 
-    products.forEach(function (product) {
-        $('<li></li>')
+    products.forEach(function(product) {
+        var newProduct = $('<li></li>')
             .html(product.getDescription())
             .attr('id', product.getId())
-            .dblclick(addToCart)
-            .appendTo("#products");
+            .dblclick(addToCart);
+        $("#products").append(newProduct);
     });
 })();
