053370140c8485175ec7b65869ff84c680f5e3f4
alex_santos <alex.santos@processmaker.com>
2017-01-06 12:02:59 -0400

    solid-ref

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
 function Product(id, description) {
-    this.getId = function () {
+    this.getId = function() {
         return id;
     };
-    this.getDescription = function () {
+    this.getDescription = function() {
         return description;
     };
 }
 
-function Cart(eventAggregator) {
+function Cart() {
     var items = [];
 
-    this.addItem = function (item) {
+    this.addItem = function(item) {
         items.push(item);
     };
-}
 
-var products = [
-new Product(1, "Star Wars Lego Ship"),
-new Product(2, "Barbie Doll"),
-new Product(3, "Remote Control Airplane")],
-    cart = new Cart();
-
-(function () {
-    function addToCart() {
-        var productId = $(this).attr('id');
-        var product = $.grep(products, function (x) {
-            return x.getId() == productId;
-        })[0];
-        cart.addItem(product);
-
+    this.eventAggregator = function(product) {
         var newItem = $('<li></li>')
             .html(product.getDescription())
             .attr('id-cart', product.getId())
             .appendTo("#cart");
+    };
+}
+
+var products = [
+        new Product(1, "Star Wars Lego Ship"),
+        new Product(2, "Barbie Doll"),
+        new Product(3, "Remote Control Airplane")
+    ],
+    cart = new Cart();
+
+(function() {
+    function addToCart() {
+        var productId = $(this).attr('id'),
+            product = $.grep(products, function(x) {
+                return x.getId() == productId;
+            })[0];
+            
+        cart.addItem(product);
+        cart.eventAggregator(product);
     }
 
-    products.forEach(function (product) {
+    products.forEach(function(product) {
         $('<li></li>')
             .html(product.getDescription())
             .attr('id', product.getId())
